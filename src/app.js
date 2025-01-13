14ea98b38e2d0955fd73a634e476b7af17f67701
Santino Strupini <strupinisantino@gmail.com>
2025-01-13 08:32:22 -0300

    sprint 7

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authUser = require('./middlewares/authUser');
 
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(cors());
 
 
 app.use(express.urlencoded({ extended: true }));
